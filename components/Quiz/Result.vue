76d1ad81cb4d6c6f5fc7465349810421513543ff
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-06-23 16:03:01 +0200

    feat(quiz): handle result

--- components/Quiz/Result.vue
+++ components/Quiz/Result.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  result: { type: Number, require: true },
+})
+</script>
 
 <template>
   <div class="result">
-    <div class="title">You got sample result 1!</div>
+    <div class="title">You got sample result {{ result }}!</div>
     <div class="desc">Enter a short description here about the result.</div>
   </div>
 </template>
